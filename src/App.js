7c9a56eec1db1019a2c4b1c5d0c4c78bba93e21f
Sohel-788 <mahmud3724@gmail.com>
2022-02-16 00:27:40 -0600

    mui drawer core2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Drawer from './Drawer';
+
+function App() {
+  return (
+    <div className="App">
+      <Drawer></Drawer>
+    </div>
+  );
+}
+
+export default App;
